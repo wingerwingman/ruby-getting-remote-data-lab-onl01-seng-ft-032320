2805a5016d4078e5b3643c308bbec5e21a4658d6
Justin Winger <justinwinger4@gmail.com>
2020-04-09 16:52:05 +0000

    Automatically backed up by Learn

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -1,1 +1,21 @@
-# Write your code here
+require 'open-uri'
+require 'net/http'
+
+class GetRequester
+  attr_accessor :url
+  
+  def initialize(url)
+    @url = url
+  end
+  
+  def get_response_body 
+     uri = URI.parse(@url)
+     response = Net::HTTP.get_response(uri)
+     response.body
+  end
+  
+  def parse_json
+    
+  end
+  
+end 
